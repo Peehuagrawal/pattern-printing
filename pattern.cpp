79776c017818313134d4a026efb4129df98af713
Peehu Agrawal <agrawalpeehu5@gmail.com>
2025-03-12 11:27:54 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    for(int i=0;i<n;i++){
+        for(char j='A';j<='D';j++){
+            cout<<j<<"\t";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
